71dd040e27118b40bcb502ef1e2a36e5b7ae27cf
Miguel Dominguez <migydgz@gmail.com>
2017-09-23 16:29:27 -0700

    my god that was close

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,11 +1,14 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
 
+import { FormPage } from '../form/form';
+
 @Component({
   selector: 'page-home',
   templateUrl: 'home.html'
 })
 export class HomePage {
+  formPage = FormPage;
 
   constructor(public navCtrl: NavController) {
 
